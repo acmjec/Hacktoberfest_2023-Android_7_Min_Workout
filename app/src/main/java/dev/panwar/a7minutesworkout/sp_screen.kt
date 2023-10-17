1ad69cadf07b0b218707bd2965b69a0533367a37
Monu <monusinghbharadwaj@gmail.com>
2023-10-17 14:28:14 +0530

    UI Improvements #8

--- app/src/main/java/dev/panwar/a7minutesworkout/sp_screen.kt
+++ app/src/main/java/dev/panwar/a7minutesworkout/sp_screen.kt
@@ -4,29 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.widget.ImageView
+import android.os.Looper
+
+
 
 
 class sp_screen : AppCompatActivity() {
-        private val splashTimeOut: Long = 3000 // 3 seconds
+        private val splashTimeOut: Long = 4000 // 4 seconds
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_sp_screen)
 
             // Add any animation code here
+            supportActionBar?.hide()
 
-            Handler().postDelayed({
+            Handler(Looper.getMainLooper()).postDelayed({
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 finish()
             }, splashTimeOut)
-            val splashImageView = findViewById<ImageView>(R.id.splashImageView)
-            splashImageView.alpha = 0f
-            splashImageView.animate().setDuration(1500).alpha(1f).withEndAction {
+
+//            val splashImageView = findViewById<ImageView>(R.id.splashImageView)
+//            splashImageView.alpha = 0f
+//            splashImageView.animate().setDuration(1500).alpha(1f).withEndAction {
                 // Animation complete
             }
 
-        }
-    }
+            }
+
+
+
 
